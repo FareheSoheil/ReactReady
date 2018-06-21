b6c96758b7c3c465d9795e1de3babb6fe0bbfeb1
Farehe Soheil <farehe.soheil@gmail.com>
2018-06-21 19:10:41 +0330

    no-html-webpack-plugin

--- src/app/Components/App.js
+++ src/app/Components/App.js
@@ -4,7 +4,7 @@ import './App.css';
 class App extends React.Component {
     render(){
         return (
-            <div className="app">This file as where you put all your final components</div>
+            <div className="app">This file is where you put all your final components</div>
         );
     }
 }
